2bbb2b48f4ce362911712c93bf23bb853fa2c1f3
vigneshdfc <vigneshdfc02@gmail.com>
2023-09-26 18:50:52 +0530

    updated

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -1,7 +1,7 @@
 import * as Yup from 'yup'
 import { Formik } from 'formik'
 import styled from 'styled-components/native'
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { View, Modal, StyleSheet, Pressable } from 'react-native'
 import { sendEmailVerification } from 'firebase/auth'
@@ -49,29 +49,11 @@ const SignupModal: React.FC<SignupModalProps> = ({ isVisible, onClose, onLoginCl
   const updateUser = userStore((state) => state.updateUser)
   const user = userStore((state) => state.user)
   const updateFetching = userStore((state) => state.updateFetching)
-  const { setMail } = userStore()
-  const { name, updateUserName } = userStore((state) => ({
-    name: state.name,
-    updateUserName: state.updateUserName,
-  }))
-  console.log('name', name)
-  console.log('updateUsername', updateUserName)
-
   const navigation = useNavigation()
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword)
   }
-
-  // const handleVerify = async () => {
-  //   if (!user) {
-  //     console.error('User is null. Cannot send email verification.')
-  //     return
-  //   }
-
-  //   await sendEmailVerification(user)
-  //   console.log('Verify')
-  // }
 
   const handleVerify = async () => {
     if (!user) {
@@ -120,8 +102,7 @@ const SignupModal: React.FC<SignupModalProps> = ({ isVisible, onClose, onLoginCl
       setIsLoading(true)
       const { user } = await createUserWithEmailAndPassword(auth, values.email, values.password)
       await updateProfile(user, { displayName: values.name })
-      await AsyncStorage.setItem('mail', values.email)
-      await AsyncStorage.setItem('name', values.name) // Store the name as well
+      await AsyncStorage.setItem('user', user)
       navigation.navigate('Post')
     } catch (error) {
       if (error instanceof FirebaseError) {
@@ -134,17 +115,6 @@ const SignupModal: React.FC<SignupModalProps> = ({ isVisible, onClose, onLoginCl
       onClose?.()
     }
   }
-
-  const getData = useCallback(async () => {
-    await AsyncStorage.getItem('mail')
-    const data = await AsyncStorage.getItem('mail')
-    setMail(data)
-    console.log(data)
-  }, [])
-
-  useEffect(() => {
-    getData()
-  }, [getData])
 
   return (
     <Modal visible={isVisible} animationType='fade' transparent={true}>
--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -8,8 +8,6 @@ interface IUserData {
 
 type State = {
   user: User | null
-  name?: string | null
-  Mail?: string | null
   userData?: IUserData
   isFetching: boolean
 }
@@ -18,20 +16,14 @@ type Action = {
   updateFetching: (fetching: boolean) => void
   updateUser: (user: User | null) => void
   updateUserData: (user: IUserData) => void
-  setMail: (Mail?: string | null) => void
-  updateUserName: (name: string) => void
 }
 
 // Create your store, which includes both state and (optionally) actions
 
 export const userStore = create<State & Action>((set) => ({
   user: null,
-  name: null,
-  Mail: '',
   isFetching: true,
   updateFetching: (fetching) => set(() => ({ isFetching: fetching })),
   updateUser: (user) => set(() => ({ user })),
   updateUserData: (userData) => set(() => ({ userData })),
-  setMail: (Mail) => set(() => ({ Mail })),
-  updateUserName: (name) => set(() => ({ name })),
 }))
