a255b4e42e530f5c91e5de905826c5344d0bed8d
vigneshdfc <vigneshdfc02@gmail.com>
2023-09-27 18:39:36 +0530

    chore: premium page updated

--- src/components/PremiumComponent/PremiumDetails.tsx
+++ src/components/PremiumComponent/PremiumDetails.tsx
@@ -7,6 +7,7 @@ import LeftArrow from '../../assets/icons/LeftArrow'
 import ShareArrow from '../../assets/icons/ShareArrow'
 import { COLORS, FONT_FAMILY } from '../../styles/theme'
 import PlayCircleIcon from '../../assets/icons/PremiumPageIcon/PlayCircle'
+import { Svg, Circle } from 'react-native-svg'
 import CustomButton from '../Button'
 
 interface IPremiumDetails {
@@ -54,47 +55,68 @@ const PremiumDetails: React.FC<IPremiumDetails> = ({ navigation }) => {
       {PremiumDetailsData.map((f, index) => {
         return (
           <PremiumDetailsWrapper key={index}>
-            <View>
-              <Image source={f.image} style={{ width: 196, height: 320, resizeMode: 'cover' }} />
-            </View>
-            <View>
-              <ProductText>{f.product}</ProductText>
-              <ProductName>{f.productName}</ProductName>
-              <View style={{ marginVertical: 16 }}>
-                <ProductText>{f.size}</ProductText>
-                <ProductName>{f.productSize}</ProductName>
+            <PremiumDetailsContent>
+              <View>
+                <Image source={f.image} style={{ width: 196, height: 320, resizeMode: 'cover' }} />
               </View>
-              <View style={{ marginBottom: 16 }}>
-                <ProductText>{f.material}</ProductText>
-                <ProductName>{f.wool}</ProductName>
-                <ProductName>{f.mohair}</ProductName>
-              </View>
-              <ProductText>{f.price}</ProductText>
-              <View style={{ display: 'flex', flexDirection: 'row', gap: 6, alignItems: 'center' }}>
-                <OldPriceText>
-                  {f.oldPrice} {f.inr}
-                </OldPriceText>
-                <View>
-                  <ProductName>
-                    {f.newPrice} {f.inr}
-                  </ProductName>
+              <View>
+                <ProductText>{f.product}</ProductText>
+                <ProductName>{f.productName}</ProductName>
+                <View style={{ marginVertical: 16 }}>
+                  <ProductText>{f.size}</ProductText>
+                  <ProductName>{f.productSize}</ProductName>
                 </View>
+                <View style={{ marginBottom: 16 }}>
+                  <ProductText>{f.material}</ProductText>
+                  <ProductName>{f.wool}</ProductName>
+                  <ProductName>{f.mohair}</ProductName>
+                </View>
+                <ProductText>{f.price}</ProductText>
+                <View
+                  style={{ display: 'flex', flexDirection: 'row', gap: 6, alignItems: 'center' }}
+                >
+                  <OldPriceText>
+                    {f.oldPrice} {f.inr}
+                  </OldPriceText>
+                  <View>
+                    <ProductName>
+                      {f.newPrice} {f.inr}
+                    </ProductName>
+                  </View>
+                </View>
+                <WatchVideoBorder>
+                  <PlayCircleIcon width={12} height={12} />
+                  <WatchVideoText>Watch video</WatchVideoText>
+                </WatchVideoBorder>
               </View>
-              <WatchVideoBorder>
-                <PlayCircleIcon width={12} height={12} />
-                <WatchVideoText>Watch video</WatchVideoText>
-              </WatchVideoBorder>
-            </View>
+            </PremiumDetailsContent>
+            <DetailsHeading>Detailed features</DetailsHeading>
+            {f.detailsPara.map((para, paraIndex) => (
+              <View
+                key={paraIndex}
+                style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 4 }}
+              >
+                <Svg width={8} height={8}>
+                  <Circle cx={3} cy={3} r={3} fill='rgba(70, 45, 133, 0.6)' />
+                </Svg>
+                <DetailsParaText style={{ marginLeft: 8 }}>{para}</DetailsParaText>
+              </View>
+            ))}
           </PremiumDetailsWrapper>
         )
       })}
-      <CustomButton
-        text='Add more'
-        fontFamily='Arvo-Regular'
-        fontSize={12}
-        onPress={() => navigation.navigate('PremiumThreeSixtyDegreePage')}
-        style={{ marginBottom: 30 }}
-      />
+      {/* <Btns>
+        <HideDetailsBorder>
+          <HideDetailsText>Hide details</HideDetailsText>
+        </HideDetailsBorder>
+        <CustomButton
+          text='Add more'
+          fontFamily='Arvo-Regular'
+          fontSize={13}
+          style={{ width: 170 }}
+          onPress={() => navigation.navigate('PremiumThreeSixtyDegreePage')}
+        />
+      </Btns> */}
     </LinearGradient>
   )
 }
@@ -105,13 +127,42 @@ const FlexContent = styled.View`
   align-items: center;
   justify-content: space-between;
 `
-const PremiumDetailsWrapper = styled.View`
+
+const Btns = styled.View`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  margin-bottom: 16px;
+  gap: 8px;
+  margin-top: 8px;
+  margin-bottom: 4px;
 `
+
+const DetailsHeading = styled.Text`
+  font-size: 14px;
+  font-family: ${FONT_FAMILY.ArvoRegular};
+  color: ${COLORS.iconsHighlightClr};
+  margin-bottom: 8px;
+`
+
+const PremiumDetailsWrapper = styled.View``
+
+const PremiumDetailsContent = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 8px;
+`
+
+const DetailsParaText = styled.Text`
+  font-size: 12px;
+  color: rgba(70, 45, 133, 0.6);
+  letter-spacing: -0.24px;
+  line-height: 16px;
+  font-family: ${FONT_FAMILY.GilroyRegular};
+`
+
 const ProductText = styled.Text`
   font-size: 10px;
   font-family: ${FONT_FAMILY.MontserratRegular};
@@ -131,6 +182,15 @@ const WatchVideoBorder = styled.Pressable`
   margin-top: 16px;
 `
 
+const HideDetailsBorder = styled.View`
+  border-radius: 30px;
+  border-width: 1px;
+  border-color: ${COLORS.textSecondaryClr};
+  padding-vertical: 12px;
+  padding-horizontal: 20px;
+  width: 170px;
+`
+
 const ProductName = styled.Text`
   font-size: 14px;
   font-family: ${FONT_FAMILY.ArvoRegular};
@@ -140,6 +200,13 @@ const ProductName = styled.Text`
 const WatchVideoText = styled.Text`
   font-size: 10px;
   color: ${COLORS.textSecondaryClr};
+`
+
+const HideDetailsText = styled.Text`
+  color: ${COLORS.textSecondaryClr};
+  font-size: 13px;
+  font-family: ${FONT_FAMILY.ArvoRegular};
+  text-align: center;
 `
 
 const OldPriceText = styled.Text`
