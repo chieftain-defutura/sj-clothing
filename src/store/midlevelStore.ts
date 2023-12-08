491116c3682716055af3b031719efcba3257b466
Subaa P <103409637+Subaa2128@users.noreply.github.com>
2023-12-08 10:17:30 +0530

    chore: pull main

---
+++ src/store/midlevelStore.ts
@@ -0,0 +1,20 @@
+// import { create } from 'zustand'
+
+// type State = {
+// midlevel:{
+// isSteps:string
+// isSelectedStyle:string
+// }
+// }
+// type Action = {
+//   updateAccessory: (Accessory: boolean) => void
+
+// }
+
+// export const MidlevelStore = create<State & Action>((set) => ({
+//   midlevel: {
+
+//   },
+
+//   updateAccessory: (midlevel) => set(() => ({ midlevel })),
+// }))
