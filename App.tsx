3bc85be5b207d42bbc833412b8d4ded60a1af6a1
Subaa P <103409637+Subaa2128@users.noreply.github.com>
2023-12-12 10:16:53 +0530

    chore: pull main

--- App.tsx
+++ App.tsx
@@ -29,57 +29,64 @@ Notifications.setNotificationHandler({
 })
 
 async function sendPushNotification(expoPushToken: any) {
-  const message = {
-    to: expoPushToken,
-    sound: 'default',
-    title: 'Some title',
-    body: 'Hello world!',
-  }
+  try {
+    const message = {
+      to: expoPushToken,
+      sound: 'default',
+      title: 'Some title',
+      body: 'Hello world!',
+    }
 
-  await fetch('https://exp.host/--/api/v2/push/send', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Accept-encoding': 'gzip, deflate',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(message),
-  })
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function registerForPushNotificationsAsync() {
-  let token
+  try {
+    let token
 
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      sound: 'mySoundFile.wav',
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    })
-  }
-
-  if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync()
-    let finalStatus = existingStatus
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync()
-      finalStatus = status
+    if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        sound: 'mySoundFile.wav',
+        vibrationPattern: [0, 250, 250, 250],
+      })
     }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!')
-      return
-    }
-    token = await Notifications.getExpoPushTokenAsync({
-      projectId: Constants?.expoConfig?.extra?.eas.projectId,
-    })
-    console.log(token)
-  } else {
-    alert('Must use physical device for Push Notifications')
-  }
 
-  return token?.data
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync()
+      let finalStatus = existingStatus
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync()
+        finalStatus = status
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!')
+        return
+      }
+      token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants?.expoConfig?.extra?.eas.projectId,
+      })
+      console.log(token)
+    } else {
+      alert('Must use physical device for Push Notifications')
+    }
+
+    return token?.data
+  } catch (error) {
+    console.log('error', error)
+  }
 }
 SplashScreen.preventAutoHideAsync()
 
