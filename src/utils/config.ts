a2d15c8df249cc68ccd456d189502df0b7c0ff92
Subaa P <103409637+Subaa2128@users.noreply.github.com>
2023-12-21 11:34:03 +0530

    chore: pull main

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const PUBLISHABLE_KEY =
   'pk_live_51OHQBRSASG7ZisasoHqLWtZkgGIg1ykBjW9ZpVPvKUWxTMpHPD1PajCuoIfDTR77ZGN2hFFQKRKqM3erpkV4bIfj00sEqlFdip'
 
-export const CURRENCY_API_KEY = '0a1c782fc9edf98309831bad'
+export const CURRENCY_API_KEY = 'bf132cfcd88c95ed5ffd44b9'
 
 export const API_URL = 'https://sj-clothing-backend.cyclic.app'
