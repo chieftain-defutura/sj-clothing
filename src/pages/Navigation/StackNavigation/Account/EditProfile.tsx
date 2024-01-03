328605b26a2a227fc312dca47a3be69cfda2bdc9
vigneshdfc <vigneshdfc02@gmail.com>
2024-01-03 10:29:22 +0530

    chore: address phone number input update

--- src/pages/Navigation/StackNavigation/Account/EditProfile.tsx
+++ src/pages/Navigation/StackNavigation/Account/EditProfile.tsx
@@ -165,7 +165,7 @@ const EditProfile: React.FC<IEditProfile> = ({ navigation }) => {
           <TouchableHighlight activeOpacity={0.6} underlayColor='rgba(70, 45, 133, 0.1)'>
             <View>
               <CustomButton
-                style={{ padding: 2 }}
+                style={{ width: width / 5.8 }}
                 fontSize={11}
                 text={!editProfileDisable ? 'Loading...' : isLoading ? 'Saving' : 'Done'}
                 disabled={!editProfileDisable || isLoading}
--- src/utils/data/AccountData.ts
+++ src/utils/data/AccountData.ts
@@ -20,6 +20,12 @@ export const AccountData = [
     rightIcon: ChevronLeft,
     navigation: 'Avatar',
   },
+  {
+    leftIcon: UserIcon,
+    name: `PhoneNumber`,
+    rightIcon: ChevronLeft,
+    navigation: 'PhoneNumber',
+  },
 
   // {
   //   name: `Payment`,
